23d9f12c30b0fa2b7400a488d6116b22b4811aea
Mario Fusco <mario.fusco@gmail.com>
2019-07-03 10:03:39 +0200

    [DROOLS-4241] first rule unit implementation (#79)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * [DROOLS-4241] first rule unit implementation
    
    * wip
    
    * wip

--- kogito-api/src/main/java/org/kie/kogito/rules/impl/DataSources.java
+++ kogito-api/src/main/java/org/kie/kogito/rules/impl/DataSources.java
@@ -1,11 +1,10 @@
 /*
- * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,19 +13,12 @@
  * limitations under the License.
  */
 
-package org.kie.internal.builder.fluent;
+package org.kie.kogito.rules.impl;
 
-import org.kie.api.runtime.rule.DataSource;
+import org.kie.kogito.rules.DataStore;
 
-/**
- * See {@link DataSource}
- */
-public interface DataSourceFluent<E, U extends RuleUnitFluent> {
-
-    DataSourceFluent<E, U> addBinding(String dataSourceName);
-
-    DataSourceFluent<E, U> insert(E object);
-
-    U buildDataSource();
-
+public class DataSources {
+    public static <T> DataStore<T> create() {
+        return new ListDataSource<>();
+    }
 }
